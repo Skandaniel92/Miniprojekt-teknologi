bd52aefd0f878d94179029f75960b29d49eecf45
Skandaniel92 <dani32c4@stud.kea.dk>
2022-11-24 18:07:44 +0100

    Initial commit

---
+++ src/main/java/com/example/teknologi_spg_egen/TeknologiSpgEgenApplication.java
@@ -0,0 +1,13 @@
+package com.example.teknologi_spg_egen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeknologiSpgEgenApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TeknologiSpgEgenApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/teknologi_spg_egen/TeknologiSpgEgenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teknologi_spg_egen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeknologiSpgEgenApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
